84269381d9078c8e48bda3174e8cc71eac09a82a
Birds Chirping <afara.ninge22@gmail.com>
2023-01-27 23:45:22 +0200

    Add sign choice. Show active player.

--- app.js
+++ app.js
@@ -38,6 +38,8 @@ const Cell = (id) => {
             player.updateCells(id);
             gamePlay.checkWin(player);
             gamePlay.togglePlayer();
+            document.getElementById('sign').setAttribute('disabled', true);
+            document.getElementById('sign2').setAttribute('disabled', true);
         }
     }
 
@@ -51,7 +53,7 @@ const Cell = (id) => {
 
 const Player = (name, sign) => {
     let markedCells = [];
-
+    
     function updateCells(cellId) {
         markedCells.push(cellId);
     }
@@ -77,12 +79,34 @@ const gamePlay = (function() {
         [1, 4, 7], [2, 5, 8], [3, 6, 9],
         [1, 5, 9], [3, 5, 7]
     ];
-
     const endScreen = document.querySelector('.end-screen');
     const resetBtn = document.querySelector('.reset-btn');
+   
+    const signOption = document.getElementById('sign');
+    const signOption2 = document.getElementById('sign2');
+    signOption.addEventListener('change', _setSignChoice.bind(this, player1, player2, signOption, signOption2));
+    signOption2.addEventListener('change', _setSignChoice.bind(this, player2, player1, signOption2, signOption));
+
+    function _setSignChoice(p1, p2, optionP1, optionP2) {
+        p1.sign = optionP1.value;
+        p2.sign = optionP1.value === 'x' ? 'o' : 'x';
+        optionP1.value = p1.sign;
+        optionP2.value = p2.sign;
+    }
+
+    const divP1 = document.querySelector(".player1");
+    const divP2 = document.querySelector(".player2");
 
     function togglePlayer() {
-        activePlayer === player1 ? activePlayer = player2 : activePlayer = player1;
+        if (activePlayer === player1) {
+            activePlayer = player2;
+            divP2.classList.add('selected-player');
+            divP1.classList.remove('selected-player');
+         } else {
+            activePlayer = player1;
+            divP1.classList.add('selected-player');
+            divP2.classList.remove('selected-player');
+         }
     }
 
     function getActivePlayer() {
@@ -90,11 +114,12 @@ const gamePlay = (function() {
     }
 
     function checkWin(player) {
-        const playerCells = player.getMarkedCells();
+        let playerCells = player.getMarkedCells();
         winLines.forEach((winningLine) => {
             let merged = playerCells.concat(winningLine);
             if (new Set(merged).size === playerCells.length) {
                 _showEndScreen(`${player.name} wins`);
+                playerCells = '';
             };
         }); 
         if (playerCells.length === 5) {
@@ -114,7 +139,10 @@ const gamePlay = (function() {
     resetBtn.addEventListener('click', _newGame);
 
     function _newGame() {
+        signOption.removeAttribute('disabled');
+        signOption2.removeAttribute('disabled');
         _hideEndScreen();
+        if (activePlayer == player2) togglePlayer();
         player1.reset();
         player2.reset();
         gameBoard.reset();
